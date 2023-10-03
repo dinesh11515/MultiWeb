46e53f8a417911739cdd3e11dd889d2c456b8f31
dinesh11515 <dineshaitham2@gmail.com>
2023-10-03 08:38:43 +0530

    added walletconnect and wagmi

---
+++ frontend/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React from "react";
+import Navbar from "../Home/Navbar";
+
+const Layout = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <div>
+      {router.pathname === "/explorer" && <Navbar bgColor={"bg-[black]"} />}
+
+      {router.pathname === "/deploy" && <Navbar bgColor={"bg-[#171717]"} />}
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
