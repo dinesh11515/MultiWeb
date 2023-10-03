0d73441aa29b53259735a4dbcf987c062b1271cf
dinesh11515 <dineshaitham2@gmail.com>
2023-10-03 08:29:42 +0530

    added homepage

---
+++ frontend/components/Home/Footer.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+const Footer = () => {
+  const router = useRouter();
+  return (
+    <div className="w-full text-white px-20 mt-[150px]">
+      <h2 className="text-3xl font-semibold mb-10">Multiweb.</h2>
+      <p className="text-gray-400 mb-12">
+        Multiweb is currrently built for Decentralized Intelligence Hackathon
+        and should be used at your own risk. We take security seriously and our
+        contracts have been thoroughly tested and formally verified but bugs may
+        still exist.
+      </p>
+
+      <div className="flex justify-between font-light text-white items-center">
+        <p className="text-sm">&#169; 2023 Multiweb Inc.</p>
+        <ul className="flex gap-8 items-center text-white">
+          <li
+            onClick={() => {
+              router.push("/");
+            }}
+            className="cursor-pointer hover:scale-105"
+          >
+            Home
+          </li>
+
+          <li
+            onClick={() => {
+              router.push("/deploy");
+            }}
+            className="cursor-pointer hover:scale-105"
+          >
+            Deploy
+          </li>
+          <li
+            onClick={() => {
+              router.push("/addContract");
+            }}
+            className="cursor-pointer hover:scale-105"
+          >
+            Add Contract
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/components/Home/Navbar.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+const Navbar = ({ bgColor, isLanding = false }) => {
+  const router = useRouter();
+  return (
+    <nav className={`pt-6 ${bgColor} w-full px-20 z-10 fixed`}>
+      <ul className="flex justify-between items-center font-Poppins ">
+        <h2
+          onClick={() => {
+            router.push("/");
+          }}
+          className="text-3xl font-semibold font-Lora text-white cursor-pointer"
+        >
+          MW.
+        </h2>
+        <div className="flex items-center gap-20 text-white">
+          <Link className="hover:scale-105" href="/">
+            Home
+          </Link>
+          <Link className="hover:scale-105" href="/deploy">
+            Deploy
+          </Link>
+        </div>
+
+        {isLanding && (
+          <button
+            onClick={() => {
+              router.push("/deploy");
+            }}
+            className=" flex items-center gap-2 justify-center hover:scale-110 delay-100 transition-all ease-in-out rounded-full py-4 bg-white w-[200px] font-semibold font-Poppins"
+          >
+            Get Started
+            <span>
+              <AiOutlineArrowRight />
+            </span>
+          </button>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
